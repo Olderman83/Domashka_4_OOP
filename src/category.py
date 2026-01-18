20a46490d14a93c066146d38fe99ae07a37975f1
Павел Руцкин <pavelru163@gmail.com>
2026-01-18 16:27:47 +0400

    New_OOP5RP

--- src/category.py
+++ src/category.py
@@ -10,11 +10,31 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []
+
+        if products:
+            for product in products:
+                self.add_product(product)
 
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.__products)
 
     def __len__(self):
         """Возвращает количество товаров в категории."""
-        return len(self.products)
+        return len(self.__products)
+
+    @property
+    def products(self):
+        """Геттер для отображения товаров в заданном формате"""
+        result = []
+        for product in self.__products:
+            result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n")
+        return result
+
+    def add_product(self, product: Product):
+        """Метод для добавления товара в категорию"""
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError("Можно добавлять только объекты класса Product")
